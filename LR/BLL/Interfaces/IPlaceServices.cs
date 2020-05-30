5063f961f6fb9954602eda2394d04fd5c611eceb
Aradam <12f4e3f48b7e6bc7b@aradam.pp.ua>
2020-05-30 19:50:15 +0300

    Revert "Revert "LR 2.2""
    This reverts commit 52aa0af85a80fc447de3bdbaf75f9932380fe3fe.

--- LR/BLL/Interfaces/IPlaceServices.cs
+++ LR/BLL/Interfaces/IPlaceServices.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BLL.Interfaces
 {
-    public interface ICafeServices
+    public interface IPlaceServices
     {
     }
 }
